a16cceae6a6989932d1e03c0875992fa0d2c79d9
Grzegorz W?jcicki <grzegorz.p.wojcicki@gmail.com>
2019-04-28 05:38:53 +0200

    fixing the trickiest one - data structures/arrays

--- data_structures__arrays/solution_6.py
+++ data_structures__arrays/solution_6.py
@@ -1,11 +1,28 @@
-def arrayManipulation(n, queries):
-    # bruteforce and times out at some tests
+def arrayManipulationBruteForce(n, queries):
+    # times out some tests
     arr = [0] * n
     for q in queries:
         for i in xrange(q[0]-1, q[1]):
             arr[i] += q[2]
 
     return max(arr)
+
+
+def arrayManipulation(n, queries):
+    # based on discussions on the topic
+    arr = [0] * (n+2)
+    for i, j, val in queries:
+        arr[i] += val
+        if j+1 <= n:
+            arr[j+1] -= val
+
+    tmp, mx = 0, 0
+    for i in xrange(1, n+1):
+        tmp += arr[i]
+        if mx < tmp:
+            mx = tmp
+
+    return mx
 
 
 if __name__ == '__main__':
